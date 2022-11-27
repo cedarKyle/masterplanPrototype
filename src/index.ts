4a2406b2e147a205453fae8b6411fcae0daf5a06
cedarKyle <kyle@cedar.build>
2022-11-26 21:54:57 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 // Notes on CodeSandBox
 // if you don't see a preview when you load this page for the first time, reload the browser tab to the right
 
-import { createViewport, createSession } from "@shapediver/viewer";
+const SDV = (<any>window).SDV;
 
 (async () => {
   // create a viewport
-  const viewport = await createViewport({
-    canvas: document.getElementById("canvas") as HTMLCanvasElement,
+  const viewport = await SDV.createViewport({
+    canvas: document.getElementById("canvas"),
     id: "myViewport"
   });
-
   // create a session
-  const session = await createSession({
+  const session = await SDV.createSession({
     ticket:
       "319f14f08c1e67a874fd843acecfd321049772deb0cdb5a0dbb39385592a156e83730e45c5e7af5eab52e15b1e36d44a092f71ada1331e1935b0f25d9448af34d0add0bd5abf8984325b97ee9e6106b25216446d15a86bb18b40114df89d2f5909b08e8c8b9eeb-7516be37cb2d968a0b3c545baf3ae51e",
     modelViewUrl: "https://sdeuc1.eu-central-1.shapediver.com",
