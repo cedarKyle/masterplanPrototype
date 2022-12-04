fef027c466734373ae36dd6e3f1250d387b3199d
cedarKyle <kyle@cedar.build>
2022-12-04 11:18:44 -0600

    updates to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,19 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const TerserPlugin = require("terser-webpack-plugin");
-
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'production',
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin({
-          terserOptions: {
-            safari10: true
-          }
-        })],
-      },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: "ts-loader",
-            options: {
-                onlyCompileBundledFiles: true
-            }
-        }],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-        })
-    ]
+  entry: './src/index.ts',
+  mode: 'production',
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    }],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
