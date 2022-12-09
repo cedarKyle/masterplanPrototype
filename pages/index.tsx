186d9e6a65b58caa7c53a690754625c1ad2006de
cedarKyle <kyle@cedar.build>
2022-12-08 21:52:09 -0600

    update images

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
                 canvas: document.getElementById("canvas") as HTMLCanvasElement,
                 id: "myViewport",
                 branding: {
-                  logo: "https://iili.io/HnanT6g.png",
+                  logo: "https://iili.io/HnaqKaR.png",
                   backgroundColor: "#000000"
                 }
               });
