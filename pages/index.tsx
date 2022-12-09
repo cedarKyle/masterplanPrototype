c956676ac7fe351549e28169a42a0a6c30c5982b
cedarKyle <kyle@cedar.build>
2022-12-08 22:25:03 -0600

    add airtable iframe

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,6 +53,8 @@ export default function Home() {
             <canvas className={styles.canvas} id="canvas"></canvas>
         </div>
 
+        <iframe class="airtable-embed" src="https://airtable.com/embed/shro4GR1nFfRnqvqf?backgroundColor=blue" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
+
 
     </div>
   );
